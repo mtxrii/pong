c56a8af2710f7db9f1d3dc5699a500f9402b79a2
Enrique <edavalos@ucsc.edu>
2020-07-12 15:03:56 -0700

    skeleton functions & objects

--- static/js/game.js
+++ static/js/game.js
@@ -14,15 +14,80 @@ const net = {
     x: (canvas.width / 2) - (netWidth / 2),
     y: 0,
 
-    width: netWidth,
+    width:  netWidth,
     height: netHeight,
     
     color: "#FFF"
 };
 
+const player = {
+    x: 10,
+    y: (canvas.height / 2) - (paddleHeight / 2),
+
+    width:  paddleWidth,
+    height: paddleHeight,
+
+    color: "#FFF",
+    score: 0
+};
+
+const opponent = {
+    x: (canvas.width) - (paddleWidth + 10),
+    y: (canvas.height / 2) - (paddleHeight / 2),
+
+    width:  paddleWidth,
+    height: paddleHeight,
+
+    color: "#FFF",
+    score: 0
+};
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+
+    radius: 7,
+    speed:  7,
+
+    velocityX: 5,
+    velocityY: 5,
+
+    color: "#05EDFF"
+  };
+
 function render() {
     context.fillStyle = "#000";
     context.fillRect(0, 0, canvas.width, canvas.height);
 }
+
+function drawNet() {
+    context.fillStyle = net.color;
+    context.fillRect(net.x, net.y, net.width, net.height);
+}
+
+function drawScore(x, y, score) {
+    context.fillStyle = "#FFF";
+    context.font = "35px sans-serif";
+
+    context.fillText(score, x, y);
+}
+
+function drawPaddle(x, y, width, height, color) {
+    context.fillStyle = color;
+    context.fillRect(x, y, width, height);
+}
+
+function drawBall(x, y, radius, color) {
+    context.fillStyle = color;
+    context.beginPath()
+
+    context.arc(x, y, radius, 0, Math.PI*2, true);
+
+    context.closePath();
+    context.fill();
+}
+
+
+
 
 render();
